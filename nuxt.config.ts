d5e41d79cf175d2aae9c00771d7e532d7417fd20
Niko Chaffin-Chicas <niko.chaffinchicas@gmail.com>
2023-10-07 11:23:14 -0700

    🛠️ Fix favicon path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         { hid: 'description', name: 'description', content: '' }
       ],
       link: [
-        { rel: 'icon', type: 'image/png', href: '/nikochaffin/favicon.png' }
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
       ]
     },
   },
