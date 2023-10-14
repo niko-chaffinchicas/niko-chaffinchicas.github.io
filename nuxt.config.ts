c28cd707f8235883ddd4302467cf0fd71d9c4112
Niko Chaffin-Chicas <niko.chaffinchicas@gmail.com>
2023-10-14 09:18:54 -0700

    Use --dotfiles flag with gh-pages to include .nojekyll, set CNAME and .nojekyll beforee generate instead of after

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    buildAssetsDir: 'nuxtAssets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
     head: {
       title: 'Niko Chaffin | Software Engineer',
       meta: [
@@ -14,9 +13,6 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', href: '/favicon.png' }
       ]
     },
-  },
-  experimental: {
-    payloadExtraction: false, // Can't load the _payload.json file, so disable it
   },
   nitro: {
     serveStatic: true,
